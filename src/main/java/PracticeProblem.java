2514fba8cb3603b4bce317b7825b338c33a7f86a
jonathan-zhao-ics4u1-03-25 <jonathan.zhao@student.tdsb.on.ca>
2025-03-21 17:40:07 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,43 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static void selectionSortName(String[] names, int[] ages){
+		for (int j = 0; j < names.length-1; j++){
+			int smallestIndex = j;
+			for (int i = j + 1; i < names.length;i++){
+				if (names[i].compareToIgnoreCase(names[smallestIndex]) < 0){
+					smallestIndex = i;
+				}
+			}
+			String temp = names[smallestIndex];
+			int tempAges = ages[smallestIndex];
+			ages[smallestIndex] = ages[j];
+			ages[j] = tempAges;
+			names[smallestIndex] = names[j];
+			names[j] = temp;
 
-}
+		}
+	}
+	
+	public static void selectionSortAge(String[] names, int[] ages){
+		for (int j = 0; j < ages.length-1; j++){
+			int smallestIndex = j;
+			for (int i = j + 1; i < ages.length; i++){
+				if (ages[i] < ages[smallestIndex]){
+					smallestIndex = i;
+				}
+			}
+			String temp = names[smallestIndex];
+			int tempAges = ages[smallestIndex];
+			ages[smallestIndex] = ages[j];
+			ages[j] = tempAges;
+			names[smallestIndex] = names[j];
+			names[j] = temp;
+			
+		}
+	}
+	
+	}
+	
+			
+
